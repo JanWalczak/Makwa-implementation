515536fd2fb2642208d0d5d56cfb23f275501ca8
Jan Walczak <jan.walczak4.stud@pw.edu.pl>
2023-03-06 21:24:17 +0100

    Final

---
+++ src/Utility.java
@@ -0,0 +1,50 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
+
+public class Utility {
+
+
+    public static byte[] HexStringToByteArray(String s) {
+
+        BigInteger tmp = new BigInteger(s,16);
+
+        return tmp.toByteArray();
+    }
+
+    public static BigInteger ByteArrayToBigInteger(byte[] data) {
+        return new BigInteger(data);
+    }
+
+    public static byte[] BigIntegerToByteArray(BigInteger bigInt){
+        return bigInt.toByteArray();
+    }
+
+
+    public static byte[] ConcatenateByteArrays(List<byte[]> arrays){
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        if(arrays!=null){
+            for(byte[] b: arrays){
+                if(b!=null){
+                    try {
+                        outputStream.write( b );
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+
+        return outputStream.toByteArray( );
+
+    }
+
+    public static String BigIntegerToHexString(BigInteger bigInt){
+        return bigInt.toString(16);
+    }
+
+}
